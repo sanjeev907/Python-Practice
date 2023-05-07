4d573f9ef585eb5313d27eef33f7daca857f5adb
sanjeev907 <53852894+sanjeev907@users.noreply.github.com>
2023-05-07 13:16:56 +0530

    first commit

--- abtraction.py
+++ abtraction.py
@@ -1,4 +1,4 @@
-# Abstarction is process of hiding the  implementationsdetails from the user and provide only highlighted services to the user
+# Abstarction is process of hiding the  implementations details from the user and provide only highlighted services to the user
 
 
 # topic Abstract Class 
@@ -9,25 +9,42 @@
     #  2. python provide the abc module to work on abstract class 
     #  3. we use @abstactmethod decorator to define the abstact method.
 
-from abc import ABC,abstractmethod
-class car(ABC):
-    def show(self):
-        print("Every car have 4 whell")
-        @abstractmethod
-        def speed(self):
-            pass
+# from abc import ABC,abstractmethod
+# class car(ABC):
+#     def show(self):
+#         print("Every car have 4 whell")
+#         @abstractmethod
+#         def speed(self):
+#             pass
 
-class Maruti(car):
-    def speed(self):
-        print("th speed of maruti is 100/km/hr.")
+# class Maruti(car):
+#     def speed(self):
+#         print("th speed of maruti is 100/km/hr.")
 
-class Tata(car):
-    def speed(self):
-        print("th speed of maruti is 180/km/hr.")
+# class Tata(car):
+#     def speed(self):
+#         print("th speed of maruti is 180/km/hr.")
 
 
-obj = Maruti()
-obj.show()
-obj.speed()
-obj1 = Tata()
-obj1.speed()
+# obj = Maruti()
+# obj.show()
+# obj.speed()
+# obj1 = Tata()
+# obj1.speed()
+
+
+
+# from abc import ABC,abstractmethod
+# class A(ABC):
+#     def show(self):
+#         print("hello world")
+#         @abstractmethod
+#         def fun1():
+#          pass
+        
+# class B(A):
+#     def fun(self):
+#         print("hello")
+
+# obj = B()
+# obj.show()
--- polymorphism.py
+++ polymorphism.py
@@ -25,15 +25,42 @@
 
 # 2. Method overriding (whenever we write method name with same signature in parent and child class called method overriding)
 
-class parent:
-    def show(self):
-        print("This is the parent class:")
+# class parent:
+#     def show(self):
+#         print("This is the parent class:")
 
-class Child(parent):
-    def show(self):
-        super().show()
-        print("This is the child class:")
+# class Child(parent):
+#     def show(self):
+#         super().show()
+#         print("This is the child class:")
 
 
-obj1 = Child()
-obj1.show()
+# obj1 = Child()
+# obj1.show()
+
+
+# 3.mutithreading
+# mutithreading is a techique which allowa a cpu to execute mutiple-threads one process at teh same time .
+
+# why mutithreading:-
+# the purpose of the muti threading is to run mutiple task and functions at the same time  
+
+# from time import sleep
+# from threading import Thread
+# class A(Thread):
+#     def run(self):
+#         for i in range(5):
+#             print("sanjeev")
+#             sleep(1)
+
+# class B(Thread):
+#     def run(self):
+#         for i in range(5):
+#             print("kaushik")
+#             sleep(1)
+
+# t1 = A()
+# t1.start()
+# t2 = B()
+# t2.start()
+
